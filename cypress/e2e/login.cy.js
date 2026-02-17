80d210f5891cacdb307307720557726c054928ae
Andressa Evilin <andressaestudos2024@gmail.com>
2026-02-17 14:36:29 -0300

    Adicionando fixtures e organização dos testes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,55 +1,45 @@
-describe('FakeStore API - Auth Login', () => {
+import loginData from "../fixtures/loginData.json";
 
-    it('[QFSA-20] POST /auth/login - credenciais válidas - deve retornar 201 e token válido', () => {
-        const user = {
-            username: "kevinryan",
-            password: "kev02937@"
-        }
+describe("FakeStore API - Auth Login", () => {
+  const endpoint = "/auth/login";
 
-        cy.request({
-            method: 'POST',
-            url: '/auth/login',
-            body: user
-        }).then(response => {
-           expect(response.status).to.eq(201);
+  describe("Cenário positivos", () => {
+    it("[QFSA-20] POST /auth/login - credenciais válidas - deve retornar 201 e token válido", () => {
+      cy.request({
+        method: "POST",
+        url: endpoint,
+        body: loginData.validUser,
+      }).then((response) => {
+        expect(response.status).to.eq(201);
 
-           expect(response.body).to.have.property('token');
-           expect(response.body.token).to.not.be.empty;
-           expect(response.body.token).to.be.a('string');
+        expect(response.body).to.have.property("token");
+        expect(response.body.token).to.not.be.empty;
+        expect(response.body.token).to.be.a("string");
+      });
+    });
+  });
 
-        })
-    }) 
+  describe("Cenários negativos", () => {
+    it("[QFSA-21] POST /auth/login - senha incorreta - deve retornar 401", () => {
+      cy.request({
+        method: "POST",
+        url: endpoint,
+        body: loginData.invalidPassword,
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(401);
+      });
+    });
 
-    it('[QFSA-21] POST /auth/login - senha incorreta - deve retornar 401', () => {
-        const user = {
-            username: "kevinryan",
-            password: "senhaErrada"
-        }
-
-        cy.request({
-            method: 'POST',
-            url: '/auth/login',
-            body: user,
-            failOnStatusCode: false
-        }).then(response => {
-           expect(response.status).to.eq(401);
-        })
-    }) 
-
-
-    it('[QFSA-23] POST /auth/login - corpo de requisição vazio - deve retornar 400', () => {
-        const user = {
-            username: "",
-            password: ""
-        }
-
-        cy.request({
-            method: 'POST',
-            url: '/auth/login',
-            body: user,
-            failOnStatusCode: false
-        }).then(response => {
-           expect(response.status).to.eq(400);
-        })
-    }) 
-})
+    it("[QFSA-23] POST /auth/login - corpo de requisição vazio - deve retornar 400", () => {
+      cy.request({
+        method: "POST",
+        url: endpoint,
+        body: loginData.emptyBody,
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(400);
+      });
+    });
+  });
+});
--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -1,125 +1,106 @@
-describe('FakeStore API - Products', () => {
-  it('[QFSA-5] GET /products - deve retornar status 200', () => {
-    cy.getAllProducts()
-    .then(response => {
-      expect(response.status).to.equal(200)
-    })
-  })
+import productData from "../fixtures/productData.json";
 
-  it('[QFSA-6] GET /products - deve retornar um array de produtos', () => {
-    cy.getAllProducts()
-    .then(response => {
-      expect(response.body).to.be.an('array')
-      expect(response.body[1]).to.be.an('object')
-    })
-  })
+describe("FakeStore API - Products", () => {
+  describe("GET /products", () => {
+    it("[QFSA-5] deve retornar status 200", () => {
+      cy.getAllProducts().then((response) => {
+        expect(response.status).to.equal(200);
+      });
+    });
 
-  it('[QFSA-7] GET /products/:id - deve retornar todos os campos obrigatórios', () => {
-    cy.getProductById(1)
-    .then(response => {
+    it("[QFSA-6] deve retornar um array de produtos", () => {
+      cy.getAllProducts().then((response) => {
+        expect(response.body).to.be.an("array");
+        expect(response.body[1]).to.be.an("object");
+      });
+    });
 
-      expect(response.status).to.eq(200)
-      
-      const product = response.body
+    it("[QFSA-7] /products/:id - deve retornar todos os campos obrigatórios", () => {
+      cy.getProductById(1).then((response) => {
+        expect(response.status).to.eq(200);
 
-      expect(product).to.have.property('id')
-      expect(product).to.have.property('title')
-      expect(product).to.have.property('price')
-      expect(product).to.have.property('description')
-      expect(product).to.have.property('category')
-      expect(product).to.have.property('image')
-      expect(product).to.have.property('rating')
-      expect(product.rating).to.have.property('rate')
-      expect(product.rating).to.have.property('count')
-    })
-  })
+        const product = response.body;
 
-  it('[QFSA-8] GET /products/:id - deve validar tipos de dados dos campos', () => {
-    cy.getProductById(1)
-    .then(response => {
-      const product = response.body
+        expect(product).to.have.property("id");
+        expect(product).to.have.property("title");
+        expect(product).to.have.property("price");
+        expect(product).to.have.property("description");
+        expect(product).to.have.property("category");
+        expect(product).to.have.property("image");
+        expect(product).to.have.property("rating");
+        expect(product.rating).to.have.property("rate");
+        expect(product.rating).to.have.property("count");
+      });
+    });
 
-      expect(product.id).to.be.a('number')
-      expect(product.title).to.be.a('string')
-      expect(product.price).to.be.a('number')
-      expect(product.description).to.be.a('string')
-      expect(product.category).to.be.a('string')
-      expect(product.image).to.be.a('string')
-      expect(product.rating).to.be.an('object')
-      
-      expect(product.rating.rate).to.be.a('number')
-      expect(product.rating.count).to.be.a('number')
-    })
-  })
+    it("[QFSA-8] /products/:id - deve validar tipos de dados dos campos", () => {
+      cy.getProductById(1).then((response) => {
+        const product = response.body;
 
-  it('[QFSA-10] GET /products - deve retornar status 200 e tempo de resposta < 2000ms', () => {
-    cy.getAllProducts()
-    .then(response => {
-      expect(response.status).to.eq(200)
-      expect(response.duration).to.be.lessThan(2000)
-    })
-  })
+        expect(product.id).to.be.a("number");
+        expect(product.title).to.be.a("string");
+        expect(product.price).to.be.a("number");
+        expect(product.description).to.be.a("string");
+        expect(product.category).to.be.a("string");
+        expect(product.image).to.be.a("string");
+        expect(product.rating).to.be.an("object");
 
-  it('[QFSA-18] GET /products/:id - deve retornar produto com ID válido', () => {
-    cy.getProductById(1)
-    .then(response => {
-        expect(response.status).be.eq(200)
-        expect(response.body.id).to.equal(1)
-        expect(response.body).be.an('object')
-    })
-})
+        expect(product.rating.rate).to.be.a("number");
+        expect(product.rating.count).to.be.a("number");
+      });
+    });
 
-it('[QFSA-19] GET /products/:id - deve retornar corpo vazio para ID inexistente (limitação da API)', () => {
+    it("[QFSA-10] deve retornar status 200 e tempo de resposta < 2000ms", () => {
+      const start = Date.now()
+      cy.getAllProducts().then((response) => {
+        const duration = Date.now() - start;
+        expect(response.status).to.eq(200);
+        expect(duration).to.be.lessThan(2000);
+      });
+    });
 
-  cy.getProductById(9999).then(response => {
+    it("[QFSA-18] GET /products/:id - deve retornar produto com ID válido", () => {
+      cy.getProductById(1).then((response) => {
+        expect(response.status).be.eq(200);
+        expect(response.body.id).to.equal(1);
+        expect(response.body).to.be.an("object");
+      });
+    });
 
-    expect(response.status).to.eq(200)
-    expect(response.body).to.be.empty
+    it("[QFSA-19] GET /products/:id - deve retornar corpo vazio para ID inexistente (limitação da API)", () => {
+      cy.getProductById(9999).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.be.empty;
+      });
+    });
+  });
 
-    // A API não retorna erro HTTP, então adaptei validação para verificar conteúdo da resposta.
-  })
+  describe("POST /products", () => {
+    it("[QFSA-25] deve criar produto válido", () => {
+      const novoProduto = productData.ValidProduct;
+      cy.addNewProduct(novoProduto).then((response) => {
+        expect(response.status).to.eq(201);
 
-})
+        expect(response.body).to.be.an("object");
 
-it('[QFSA-25] POST /products - deve criar produto válido', () => {
-  const novoProduto = {
-    title: 'string',
-    price: 0.1,
-    description: 'string',
-    category: 'string',
-    image: "http://example.com"
-  }
-  
+        expect(response.body).to.have.property("id");
+        expect(response.body).to.have.property("title");
+        expect(response.body).to.have.property("price");
+        expect(response.body).to.have.property("category");
+        expect(response.body).to.have.property("image");
 
-  cy.addNewProduct(novoProduto).then(response => {
-    expect(response.status).to.eq(201)
+        expect(response.body.title).to.eq(novoProduto.title);
+        expect(response.body.price).to.eq(novoProduto.price);
+        expect(response.body.description).to.eq(novoProduto.description);
+      });
+    });
 
-    expect(response.body).to.be.an('object')
+    it("[QFSA-26] deve permitir criação sem campos obrigatórios (limitação da API)", () => {
+      const novoProduto = productData.InvalidProduct;
 
-    expect(response.body).to.have.property('id')
-    expect(response.body).to.have.property('title')
-    expect(response.body).to.have.property('price')
-    expect(response.body).to.have.property('category')
-    expect(response.body).to.have.property('image')
-
-    expect(response.body.title).to.eq(novoProduto.title)
-    expect(response.body.price).to.eq(novoProduto.price)
-    expect(response.body.description).to.eq(novoProduto.description)
-
-  })
-})
-
-it('[QFSA-26] POST /products - deve permitir criação sem campos obrigatórios (limitação da API)', () => {
-  const novoProduto = {
-    price: 0.1,
-    description: 'string',
-  }
-
-  cy.addNewProduct(novoProduto).then(response => {
-    expect(response.status).to.eq(201) 
-  })
-})
-
-
-
-})
+      cy.addNewProduct(novoProduto).then((response) => {
+        expect(response.status).to.eq(201);
+      });
+    });
+  });
+});
